d86af6b358f207879c8168d4d25b2cba0f8a086d
anmolseth02 <31291708+anmolseth02@users.noreply.github.com>
2017-12-07 11:46:41 -0500

    test3

--- app_test.py
+++ app_test.py
@@ -1,11 +1,11 @@
 import unittest
-from main import app
+from app import app
 
 class IntegerArithmenticTestCase(unittest.TestCase):
+	def testAdd(self): 
+		self.assertEquals((1 + 2), 3)
+		self.assertEquals(0 + 1, 1)
 
-    def testAdd(self):  ## test method names begin 'test*'
-        self.assertEquals((1 + 2), 3)
-        self.assertEquals(0 + 1, 1)
 
-if _name_ == '_main_':
-    unittest.main()
+if __name__ == '__main__':
+	unittest.main()
