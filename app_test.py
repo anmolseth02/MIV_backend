25d2b4478c7d891599195c00f74d12b9d84c5e50
anmolseth02 <31291708+anmolseth02@users.noreply.github.com>
2017-12-07 14:58:10 -0500

    test comments

--- app_test.py
+++ app_test.py
@@ -2,10 +2,12 @@ import unittest
 from app import app
 
 class IntegerArithmenticTestCase(unittest.TestCase):
+
+# test 1
 	def testAdd(self): 
 		self.assertEquals((1 + 2), 3)
 		self.assertEquals(0 + 1, 1)
-
+# test 2
 	def test_two(self):
 		tester = app.test_client(self)
 		response = tester.get('/')
